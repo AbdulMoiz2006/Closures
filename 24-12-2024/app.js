d50f901e30a968897c69dfe7ed321071b38404a9
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-12-24 04:45:51 +0500

    Add files via upload

---
+++ 24-12-2024/app.js
@@ -0,0 +1,74 @@
+
+
+
+// var a = 10
+
+// var a = 20
+
+
+
+// console.log(a);
+
+
+// function x(){
+
+//     var a = 10
+
+// function y(){
+
+//     console.log(a);    
+
+// }   
+
+// y()
+
+// }
+
+// x()
+
+// console.log(a);
+
+//  var a = 10
+
+// setTimeout =>(){
+
+// }
+
+
+
+
+
+// var sum = function(a,b,c){
+
+// return a+b+c
+
+// }
+
+// var store = sum(4,5,6)
+// console.log(store)
+
+
+
+
+var sum = function(a,b,c){
+
+return {
+sumTwo:function(){
+
+return a+b
+
+},
+
+sumThree:function(){
+
+return a+b+c
+
+}
+}
+}
+
+
+
+var store = sum(4,5,6)
+console.log(store.sumTwo())
+console.log(store.sumThree()) 

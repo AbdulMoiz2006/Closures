b4091b82054dc3bb1dd34775cd2c3ea4718efa33
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-12-26 04:20:54 +0500

    Add files via upload

---
+++ var,let,const/app.js
@@ -0,0 +1,36 @@
+// const accountId = 12345
+// let accountEmail = "moiz@gmail.com"
+// var accountpassword = "54321"
+// accountCity = "Bengaluru"
+
+// accountEmail = "umayma@gmail.com"
+// accountpassword = "45454354354"
+// accountCity = "karachi"
+
+
+
+// console.table([,accountEmail,accountpassword,accountCity,accountId])
+
+
+
+// const accountId = 12345
+// let accountEmail = "moiz@gmail.com"
+// var accountpassword = "534342"
+
+// // accountId = 543221
+// accountEmail = "umayma@gmail.com"
+// accountpassword = "45454354354"
+
+
+
+// console.log(accountEmail);
+
+
+
+var  email = "moiz@gmail.com"
+
+var  email = "umajdjfdlk"
+var email = "34rdrsd"
+
+
+console.log(email);
